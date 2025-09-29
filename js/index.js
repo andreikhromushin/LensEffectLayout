808e3c2a36f7a85e7f8639cafed407727c603409
Andrei Khromushin <andrei@khromushin.org>
2025-09-30 03:06:50 +0500

    Update configs, architecture and style guide

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-document.addEventListener("mousemove", e => {
+document.addEventListener("mousemove", (e) => {
   Object.assign(document.documentElement, {
     style: `
-		--move-x: ${(e.clientX - window.innerWidth / 2) * -.005}deg;
-		--move-y: ${(e.clientY - window.innerHeight / 2) * .01}deg;
-		`
+		--move-x: ${(e.clientX - window.innerWidth / 2) * -0.005}deg;
+		--move-y: ${(e.clientY - window.innerHeight / 2) * 0.01}deg;
+		`,
   });
 });
